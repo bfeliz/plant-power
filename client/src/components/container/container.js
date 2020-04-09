6324b190b7f7de2afaed5e8879aab901f8cbb48b
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-04-09 11:13:04 -0700

    Add container

---
+++ client/src/components/container/container.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Container() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col s12"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Container;
