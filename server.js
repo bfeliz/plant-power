979e4b616ca1beaea669c84b2b64e3018b1709ec
Jim Denis <jimdenis007@yahoo.com>
2020-04-10 11:31:44 -0700

    added api calls

--- server.js
+++ server.js
@@ -13,23 +13,23 @@ app.use(express.json());
 
 // static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactrecipes",
-  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+    process.env.MONGODB_URI || "mongodb://localhost/reactrecipes",
+    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
 // Use apiRoutes
 app.use(routes);
 
 // If no API routes are hit, send the React app
-app.use(function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+//app.use(function (req, res) {
+//    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+//});
 
 app.listen(PORT, function () {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
